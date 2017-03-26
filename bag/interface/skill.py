42fe773ec3538b4efd46909bc8fae1fdda952d44
Eric Chang <pkerichang@berkeley.edu>
2017-03-25 17:33:52 -0700

    1. adding support for corner-dependent parameters. 2. adding lib_name property to TemplateDB.

--- bag/interface/skill.py
+++ bag/interface/skill.py
@@ -32,6 +32,7 @@ from builtins import *
 
 import os
 import yaml
+from typing import List, Dict, Optional, Any, Tuple
 
 from jinja2 import Template
 
@@ -146,6 +147,7 @@ class SkillInterface(DbAccess):
         self.handler.close()
 
     def _eval_skill(self, expr, input_files=None, out_file=None):
+        # type: (str, Optional[Dict[str, Any]], Optional[str]) -> str
         """Send a request to evaluate the given skill expression.
 
         Because Virtuoso has a limit on the input/output data (< 4096 bytes),
@@ -391,7 +393,8 @@ class SkillInterface(DbAccess):
         output = yaml.load(self._eval_skill(cmd, out_file='result_file'))
         return output['enabled_corners'], output['corners'], output['parameters'], output['outputs']
 
-    def update_testbench(self, lib, cell, parameters, sim_envs, config_rules):
+    def update_testbench(self, lib, cell, parameters, sim_envs, config_rules, env_parameters):
+        # type: (str, str, Dict[str, str], List[str], List[List[str]], List[List[Tuple[str, str]]]) -> None
         """Update the given testbench configuration.
 
         Parameters
@@ -400,20 +403,24 @@ class SkillInterface(DbAccess):
             testbench library.
         cell : str
             testbench cell.
-        parameters : dict[str, str]
+        parameters : Dict[str, str]
             testbench parameters.
-        sim_envs : list[str]
+        sim_envs : List[str]
             list of enabled simulation environments.
-        config_rules : list[list[str]]
+        config_rules : List[List[str]]
             config view mapping rules, list of (lib, cell, view) rules.
+        env_parameters : List[List[Tuple[str, str]]]
+            list of param/value list for each simulation environment.
         """
-        cmd = 'modify_testbench("%s" "%s" {conf_rules} {run_opts} {sim_envs} {params})' % (lib, cell)
+
+        cmd = 'modify_testbench("%s" "%s" {conf_rules} {run_opts} {sim_envs} {params} {env_params})' % (lib, cell)
         in_files = {'conf_rules': config_rules,
                     'run_opts': [],
                     'sim_envs': sim_envs,
                     'params': list(parameters.items()),
+                    'env_params': list(zip(sim_envs, env_parameters)),
                     }
-        return self._eval_skill(cmd, input_files=in_files)
+        self._eval_skill(cmd, input_files=in_files)
 
     def instantiate_layout_pcell(self, lib_name, cell_name, view_name,
                                  inst_lib, inst_cell, params, pin_mapping):
