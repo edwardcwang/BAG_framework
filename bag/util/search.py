deb44b2ba39e4f374c3e4d32fd577ea05b88f2a6
Eric Chang <pkerichang@berkeley.edu>
2017-08-08 09:50:25 -0700

    bag.util.search: 1. added FloatBinaryIterator class used to perform binary search    on floating point numbers.
    bag.data.lti:
    1. added get_stability_margin() and get_w_crossings() method for
       stability analysis purposes.

--- bag/util/search.py
+++ bag/util/search.py
@@ -34,9 +34,10 @@ from typing import Optional
 
 
 class BinaryIterator(object):
-    """A class that performs binary search over integer or float range.
+    """A class that performs binary search over integers.
 
-    This class simplifies binary search algorithm writing, whether it be bounded or unbounded.
+    This class supports both bounded or unbounded binary search, and
+    you can also specify a step size.
 
     Parameters
     ----------
@@ -113,3 +114,81 @@ class BinaryIterator(object):
         if self._save_marker is None:
             return None
         return self._save_marker * self._step + self._offset
+
+
+class FloatBinaryIterator(object):
+    """A class that performs binary search over floating point numbers.
+
+    This class supports both bounded or unbounded binary search, and terminates
+    when we can guarantee the given error tolerance.
+
+    Parameters
+    ----------
+    low : float
+        the lower bound.
+    high : Optional[float]
+        the upper bound.  None for unbounded binary search.
+    tol : float
+        we will guarantee that the final solution will be within this
+        tolerance.
+    search_step : float
+        for unbounded binary search, this is the initial step size when
+        searching for upper bound.
+    """
+
+    def __init__(self, low, high, tol=1.0, search_step=1.0):
+        # type: (float, Optional[float], float) -> None
+        self._offset = low
+        self._tol = tol
+        self._low = 0
+        self._search_step = search_step
+
+        if high is not None:
+            self._high = high - low
+            self._current = self._high / 2
+        else:
+            self._high = None
+            self._current = 0
+
+        self._save_marker = None
+
+    def has_next(self):
+        # type: () -> bool
+        """returns True if this iterator is not finished yet."""
+        return self._high is None or self._low + 2 * self._tol < self._high
+
+    def get_next(self):
+        # type: () -> float
+        """Returns the next value to look at."""
+        return self._current + self._offset
+
+    def up(self):
+        # type: () -> None
+        """Increment this iterator."""
+        self._low = self._current
+
+        if self._high is not None:
+            self._current = (self._low + self._high) / 2
+        else:
+            if self._current != 0:
+                self._current *= 2
+            else:
+                self._current = self._search_step
+
+    def down(self):
+        # type: () -> None
+        """Decrement this iterator."""
+        self._high = self._current
+        self._current = (self._low + self._high) / 2
+
+    def save(self):
+        # type: () -> None
+        """Save the current index"""
+        self._save_marker = self._current
+
+    def get_last_save(self):
+        # type: () -> Optional[float]
+        """Returns the last saved index."""
+        if self._save_marker is None:
+            return None
+        return self._save_marker + self._offset
