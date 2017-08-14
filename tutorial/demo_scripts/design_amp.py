bb182ea36585cfec333baa1472d17ccf89cbfcb5
Eric Chang <pkerichang@berkeley.edu>
2017-08-14 09:48:00 -0700

    BAG_framework/demo: 1. update design_amp_cs() method to design assuming constant current    across corners.

--- tutorial/demo_scripts/design_amp.py
+++ tutorial/demo_scripts/design_amp.py
@@ -7,11 +7,10 @@ from ckt_dsn_ec.mos.core import MOSDBDiscrete
 
 if __name__ == '__main__':
     vdd = 1.0
-    vin = vdd / 2
     vout = 0.7
     cload = 40e-15
-    fbw = 1e9
-    gain_min = 4.991
+    fbw = 4e9
+    gain_min = 4
 
     w_list = [4]
     nch_conf_list = ['BAG_framework/tutorial/data/mos_char_nch']
@@ -20,4 +19,4 @@ if __name__ == '__main__':
     nch_db = MOSDBDiscrete(w_list, nch_conf_list, 1)
     pch_db = MOSDBDiscrete(w_list, pch_conf_list, 1)
 
-    design_amp_cs(nch_db, pch_db, vdd, vin, vout, cload, fbw, gain_min)
+    design_amp_cs(nch_db, pch_db, vdd, vout, cload, fbw, gain_min)
