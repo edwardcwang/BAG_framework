6ac389b3f6cf97152aa659e9313023ffa38fc7e5
Eric Chang <pkerichang@berkeley.edu>
2017-08-21 20:58:22 -0700

    bag.data.lti.LTICircuit: 1. change add_transistor() type hint to allow float fg.
    bag.data.lti.mos:
    1. added optional cfit_method parameter to mos_y_to_ss().

--- bag/data/mos.py
+++ bag/data/mos.py
@@ -30,8 +30,8 @@ from typing import Dict
 import numpy as np
 
 
-def mos_y_to_ss(sim_data, char_freq, fg, ibias):
-    # type: (Dict[str, np.ndarray], float, int, np.ndarray) -> Dict[str, np.ndarray]
+def mos_y_to_ss(sim_data, char_freq, fg, ibias, cfit_method='average'):
+    # type: (Dict[str, np.ndarray], float, int, np.ndarray, str) -> Dict[str, np.ndarray]
     """Convert transistor Y parameters to small-signal parameters.
 
     This function computes MOSFET small signal parameters from 3-port
@@ -49,6 +49,9 @@ def mos_y_to_ss(sim_data, char_freq, fg, ibias):
         number of transistor fingers used for the Y parameter measurement.
     ibias : np.ndarray
         the DC bias current of the transistor.  Always positive.
+    cfit_method : str
+        method used to extract capacitance from Y parameters.  Currently
+        supports 'average' or 'worst'
 
     Returns
     -------
@@ -62,12 +65,30 @@ def mos_y_to_ss(sim_data, char_freq, fg, ibias):
     gds = (sim_data['y22'].real - sim_data['y32'].real) / 2.0
     gb = (sim_data['y33'].real - sim_data['y23'].real) / 2.0 - gm - gds
 
-    cgd = -0.5 / w * (sim_data['y12'].imag + sim_data['y21'].imag)
-    cgs = -0.5 / w * (sim_data['y13'].imag + sim_data['y31'].imag)
-    cds = -0.5 / w * (sim_data['y23'].imag + sim_data['y32'].imag)
-    cgb = sim_data['y11'].imag / w - cgd - cgs
-    cdb = sim_data['y22'].imag / w - cds - cgd
-    csb = sim_data['y33'].imag / w - cgs - cds
+    cgd12 = -sim_data['y12'].imag / w
+    cgd21 = -sim_data['y21'].imag / w
+    cgs13 = -sim_data['y13'].imag / w
+    cgs31 = -sim_data['y31'].imag / w
+    cds23 = -sim_data['y23'].imag / w
+    cds32 = -sim_data['y32'].imag / w
+    cgg = sim_data['y11'].imag / w
+    cdd = sim_data['y22'].imag / w
+    css = sim_data['y33'].imag / w
+
+    if cfit_method == 'average':
+        cgd = (cgd12 + cgd21) / 2
+        cgs = (cgs13 + cgs31) / 2
+        cds = (cds23 + cds32) / 2
+    elif cfit_method == 'worst':
+        cgd = np.maximum(cgd12, cgd21)
+        cgs = np.maximum(cgs13, cgs31)
+        cds = np.maximum(cds23, cds32)
+    else:
+        raise ValueError('Unknown cfit_method = %s' % cfit_method)
+
+    cgb = cgg - cgd - cgs
+    cdb = cdd - cds - cgd
+    csb = css - cgs - cds
 
     ibias = ibias / fg  # type: np.ndarray
     gm = gm / fg  # type: np.ndarray
