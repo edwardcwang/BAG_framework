7653b77093f28698a975e702ce16d7d76bca01aa
Eric Chang <pkerichang@gmail.com>
2017-11-19 21:33:27 -0800

    1. change virt_server.sh to use bash instead of tcsh. 2. added setup_submodules.sh to simplify adding git submodules. 3. added sample bag_submodules.yaml configuration file.

--- run_scripts/virt_server.sh
+++ run_scripts/virt_server.sh
@@ -1,16 +1,12 @@
-#!/bin/tcsh
+#!/usr/bin/env bash
 
-# hack to get iPDK pycells to work.  Berkeley specific setting.
-if ($?PYTHONPATH) then
-    setenv PYTHONPATH_PYCELL "${PYTHONPATH}"
-endif
-setenv PYTHONPATH "${BAG_FRAMEWORK}"
+export PYTHONPATH="${BAG_FRAMEWORK}"
 
-set cmd = "-m bag.virtuoso run_skill_server"
-set min_port = 5000
-set max_port = 9999
-set port_file = "BAG_server_port.txt"
-set log = "skill_server.log"
+export cmd="-m bag.virtuoso run_skill_server"
+export min_port=5000
+export max_port=9999
+export port_file="BAG_server_port.txt"
+export log="skill_server.log"
 
-set cmd = "${BAG_PYTHON} ${cmd} ${min_port} ${max_port} ${port_file} ${log}"
+export cmd="${BAG_PYTHON} ${cmd} ${min_port} ${max_port} ${port_file} ${log}"
 exec $cmd
