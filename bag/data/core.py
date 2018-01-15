369776479a73b4c5fd0078381c34754f715bf2c6
Eric Chang <pkerichang@gmail.com>
2018-01-14 23:17:08 -0800

    1. remove license statements from source files. 2. remove some Pycharm warnings. 3. update some import order to standard convention. 4. added standard BSD 3-Clause license file.

--- bag/data/core.py
+++ bag/data/core.py
@@ -1,26 +1,4 @@
 # -*- coding: utf-8 -*-
-########################################################################################################################
-#
-# Copyright (c) 2014, Regents of the University of California
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
-# following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
-#   disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
-#    following disclaimer in the documentation and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-########################################################################################################################
 
 """This module defines core data post-processing classes.
 """
@@ -274,6 +252,7 @@ class Waveform(object):
         best_values = None
         mid_lev = nlev // 2
         for idx, t_off in enumerate(toff_vec):
+            # noinspection PyTypeChecker
             values = self(np.arange(tstart + t_off, tend, tbit))
             values.sort()
 
@@ -329,6 +308,7 @@ class Waveform(object):
             raise ValueError("Trying to add non-Waveform object.")
         xnew = np.concatenate((self.xvec, other.xvec))
         xnew = np.unique(np.around(xnew / self.xtol)) * self.xtol
+        # noinspection PyTypeChecker
         y1 = self(xnew)
         y2 = other(xnew)
         return xnew, y1 + y2
