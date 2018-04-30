e790ebb2cf2df09cca77e4841aadafca7a3a99e6
Eric Chang <pkerichang@gmail.com>
2018-04-30 09:23:41 -0700

    1. rename BiasBusUnit to BiasShield.  This makes tiling easier.

--- routing/bias.py
+++ routing/bias.py
@@ -16,8 +16,8 @@ if TYPE_CHECKING:
     from bag.layout.template import TemplateDB
 
 
-class BiasBusUnit(TemplateBase):
-    """Unit cell template for bias bus.
+class BiasShield(TemplateBase):
+    """Unit cell template for shield around bias wires.
 
     Parameters
     ----------
@@ -37,6 +37,11 @@ class BiasBusUnit(TemplateBase):
     def __init__(self, temp_db, lib_name, params, used_names, **kwargs):
         # type: (TemplateDB, str, Dict[str, Any], Set[str], **kwargs) -> None
         TemplateBase.__init__(self, temp_db, lib_name, params, used_names, **kwargs)
+        self._bias_tids = None
+
+    @property
+    def bias_tids(self):
+        return self._bias_tids
 
     @classmethod
     def get_params_info(cls):
@@ -46,19 +51,23 @@ class BiasBusUnit(TemplateBase):
             nwire='number of routing wires.',
             width='route wire width.',
             space_sig='route wire spacing.',
+            top='True to draw top shield.',
             space_sup='supplu wire spacing.',
-            show_pins='True to show pins.',
         )
 
     @classmethod
     def get_default_param_values(cls):
         # type: () -> Dict[str, Any]
         return dict(
-            width=1,
-            space_sig=0,
+            top=True,
             space_sup=1,
-            show_pins=True,
         )
+
+    def get_layout_basename(self):
+        layer = self.params['layer']
+        nwire = self.params['nwire']
+        desc = 'top' if self.params['top'] else 'bot'
+        return 'bias_shield_%s_lay%d_n%d' % (desc, layer, nwire)
 
     def draw_layout(self):
         # type: () -> None
@@ -66,8 +75,8 @@ class BiasBusUnit(TemplateBase):
         nwire = self.params['nwire']
         width = self.params['width']
         space_sig = self.params['space_sig']
+        top = self.params['top']
         space_sup = self.params['space_sup']
-        show_pins = self.params['show_pins']
 
         if isinstance(space_sup, numbers.Integral):
             space_sup = [space_sup, space_sup]
@@ -96,32 +105,34 @@ class BiasBusUnit(TemplateBase):
         route_list = list(chain(tmp, repeat('sig', nwire), tmp))
         ntr, locs = tr_manager.place_wires(route_layer, route_list)
         par_dim = lcm([bot_pitch * (1 + space_sup[0]), top_pitch * (1 + space_sup[1])])
+        perp_dim = ntr * route_pitch
         if is_horiz:
             tr_upper = -(-par_dim // blk_w) * blk_w
-            tot_h = -(-(ntr * route_pitch) // blk_h) * blk_h
+            tot_h = -(-perp_dim // blk_h) * blk_h
             bbox = BBox(0, 0, tr_upper, tot_h, res, unit_mode=True)
 
         else:
             tr_upper = -(-par_dim // blk_h) * blk_h
-            tot_w = -(-(ntr * route_pitch) // blk_w) * blk_w
+            tot_w = -(-perp_dim // blk_w) * blk_w
             bbox = BBox(0, 0, tot_w, tr_upper, res, unit_mode=True)
 
         self.set_size_from_bound_box(top_layer, bbox)
         self.array_box = bbox
 
-        for idx in range(1, len(route_list) - 1):
-            warr = self.add_wires(route_layer, locs[idx], 0, tr_upper, width=width, unit_mode=True)
-            self.add_pin('sig%d' % (idx - 1), warr, show=show_pins)
+        self._bias_tids = [TrackID(route_layer, locs[idx], width=width)
+                           for idx in range(1, nwire + 1)]
 
-        tidx = locs[0]
-        sh_warr = self.add_wires(route_layer, tidx, 0, tr_upper, num=2, pitch=locs[-1] - tidx,
+        pitch = locs[nwire + 1] - locs[0]
+        sh_warr = self.add_wires(route_layer, locs[0], 0, tr_upper, num=2, pitch=pitch,
                                  unit_mode=True)
-        sup_pitch = space_sup[0] + 1
-        sup_tid = TrackID(bot_layer, space_sup[0] / 2, num=(tr_upper // (sup_pitch * bot_pitch)),
-                          pitch=sup_pitch)
-        self.connect_to_tracks(sh_warr, sup_tid)
-        sup_pitch = space_sup[1] + 1
-        sup_tid = TrackID(top_layer, space_sup[1] / 2, num=(tr_upper // (sup_pitch * top_pitch)),
-                          pitch=sup_pitch)
-        warr = self.connect_to_tracks(sh_warr, sup_tid)
-        self.add_pin('sup', warr, show=show_pins)
+        if top:
+            sup_pitch = space_sup[1] + 1
+            sup_tid = TrackID(top_layer, space_sup[1] / 2,
+                              num=(tr_upper // (sup_pitch * top_pitch)), pitch=sup_pitch)
+            warr = self.connect_to_tracks(sh_warr, sup_tid)
+        else:
+            sup_pitch = space_sup[0] + 1
+            sup_tid = TrackID(bot_layer, space_sup[0] / 2,
+                              num=(tr_upper // (sup_pitch * bot_pitch)), pitch=sup_pitch)
+            warr = self.connect_to_tracks(sh_warr, sup_tid)
+        self.add_pin('sup', warr, show=False)
