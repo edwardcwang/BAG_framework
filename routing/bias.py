99173b6f081ce6e68f64ec8557a4457d7c51c543
Eric Chang <pkerichang@gmail.com>
2018-04-30 06:01:01 -0700

    1. update BiasBusUnit layout algorithm.

--- routing/bias.py
+++ routing/bias.py
@@ -4,8 +4,10 @@
 
 from typing import TYPE_CHECKING, Dict, Set, Any
 
+import numbers
 from itertools import chain, repeat
 
+from bag.math import lcm
 from bag.layout.util import BBox
 from bag.layout.template import TemplateBase
 from bag.layout.routing.base import TrackManager, TrackID
@@ -40,10 +42,11 @@ class BiasBusUnit(TemplateBase):
     def get_params_info(cls):
         # type: () -> Dict[str, str]
         return dict(
-            route_layer='the routing layer.',
+            layer='the routing layer.',
             nwire='number of routing wires.',
             width='route wire width.',
-            space='route wire spacing.',
+            space_sig='route wire spacing.',
+            space_sup='supplu wire spacing.',
             show_pins='True to show pins.',
         )
 
@@ -52,38 +55,54 @@ class BiasBusUnit(TemplateBase):
         # type: () -> Dict[str, Any]
         return dict(
             width=1,
-            space=0,
+            space_sig=0,
+            space_sup=1,
             show_pins=True,
         )
 
     def draw_layout(self):
         # type: () -> None
-        route_layer = self.params['route_layer']
+        route_layer = self.params['layer']
         nwire = self.params['nwire']
         width = self.params['width']
-        space = self.params['space']
+        space_sig = self.params['space_sig']
+        space_sup = self.params['space_sup']
         show_pins = self.params['show_pins']
+
+        if isinstance(space_sup, numbers.Integral):
+            space_sup = [space_sup, space_sup]
 
         res = self.grid.resolution
 
+        bot_layer = route_layer - 1
         top_layer = route_layer + 1
         is_horiz = self.grid.get_direction(route_layer) == 'x'
-        blk_w, blk_h = self.grid.get_block_size(top_layer, unit_mode=True)
+        if is_horiz:
+            half_blk_x = False
+            half_blk_y = True
+        else:
+            half_blk_x = True
+            half_blk_y = False
+        blk_w, blk_h = self.grid.get_block_size(top_layer, unit_mode=True, half_blk_x=half_blk_x,
+                                                half_blk_y=half_blk_y)
+        bot_pitch = self.grid.get_track_pitch(bot_layer, unit_mode=True)
+        top_pitch = self.grid.get_track_pitch(top_layer, unit_mode=True)
         route_pitch = self.grid.get_track_pitch(route_layer, unit_mode=True)
 
         tr_manager = TrackManager(self.grid, {'sig': {route_layer: width}},
-                                  {('sig', ''): {route_layer: space}}, half_space=True)
+                                  {('sig', ''): {route_layer: space_sig}}, half_space=True)
 
         tmp = [1]
         route_list = list(chain(tmp, repeat('sig', nwire), tmp))
         ntr, locs = tr_manager.place_wires(route_layer, route_list)
+        par_dim = lcm([bot_pitch * (1 + space_sup[0]), top_pitch * (1 + space_sup[1])])
         if is_horiz:
-            tr_upper = blk_w
+            tr_upper = -(-par_dim // blk_w) * blk_w
             tot_h = -(-(ntr * route_pitch) // blk_h) * blk_h
             bbox = BBox(0, 0, tr_upper, tot_h, res, unit_mode=True)
 
         else:
-            tr_upper = blk_h
+            tr_upper = -(-par_dim // blk_h) * blk_h
             tot_w = -(-(ntr * route_pitch) // blk_w) * blk_w
             bbox = BBox(0, 0, tot_w, tr_upper, res, unit_mode=True)
 
@@ -95,10 +114,14 @@ class BiasBusUnit(TemplateBase):
             self.add_pin('sig%d' % (idx - 1), warr, show=show_pins)
 
         tidx = locs[0]
-        sh_warr = self.add_wires(route_layer, tidx, 0, tr_upper, num=2, pitch=locs[-1] - tidx)
-        for lay in (route_layer - 1, top_layer):
-            cur_pitch = self.grid.get_track_pitch(lay, unit_mode=True)
-            ntr = tr_upper // cur_pitch
-            exp = self.connect_to_tracks(sh_warr, TrackID(lay, 0, num=ntr, pitch=1))
-
-        self.add_pin('sup', exp, show=show_pins)
+        sh_warr = self.add_wires(route_layer, tidx, 0, tr_upper, num=2, pitch=locs[-1] - tidx,
+                                 unit_mode=True)
+        sup_pitch = space_sup[0] + 1
+        sup_tid = TrackID(bot_layer, space_sup[0] / 2, num=(tr_upper // (sup_pitch * bot_pitch)),
+                          pitch=sup_pitch)
+        self.connect_to_tracks(sh_warr, sup_tid)
+        sup_pitch = space_sup[1] + 1
+        sup_tid = TrackID(top_layer, space_sup[1] / 2, num=(tr_upper // (sup_pitch * top_pitch)),
+                          pitch=sup_pitch)
+        warr = self.connect_to_tracks(sh_warr, sup_tid)
+        self.add_pin('sup', warr, show=show_pins)
