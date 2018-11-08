6a05447c74aa6d165e64dc69eabd5a6ac65caf6f
Eric Chang <pkerichang@gmail.com>
2018-11-07 19:54:25 -0800

    Remove auto-imports in bag.layout package.

--- bag/layout/__init__.py
+++ bag/layout/__init__.py
@@ -2,10 +2,3 @@
 
 """This package contains code for templated based layout.
 """
-
-from .core import TechInfo
-from .routing import RoutingGrid
-from .template import TemplateDB
-
-
-__all__ = ['TechInfo', 'RoutingGrid', 'TemplateDB', ]
