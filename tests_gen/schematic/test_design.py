d944a5a2bbcf672b4d2cd75b788ac080c1d739e1
Eric Chang <pkerichang@gmail.com>
2018-11-22 14:49:45 -0800

    Added test skipping and CDL netlist test.

--- tests_gen/schematic/test_design.py
+++ tests_gen/schematic/test_design.py
@@ -5,6 +5,7 @@ from typing import Dict, Any
 import pathlib
 
 import yaml
+import pytest
 
 from pybag.enum import DesignOutput
 
@@ -29,16 +30,20 @@ def test_design_yaml(tmpdir,
                      ) -> None:
     """Test design() method of each schematic generator."""
     expect_fname = sch_design_params.get('out_yaml', '')
+    if not expect_fname and not gen_output:
+        pytest.skip('Cannot find expected YAML output')
 
     dsn = get_sch_master(module_db, sch_design_params)
 
     path = tmpdir.join('out.yaml')
     module_db.instantiate_master(DesignOutput.YAML, dsn, top_cell_name='PYTEST', fname=str(path))
 
+    assert path.check(file=1)
+
     with path.open('r') as f:
         actual = yaml.load(f)
 
-    if gen_output or expect_fname:
+    if gen_output:
         dir_name = pathlib.Path('pytest_output')
         out_fname = dir_name / (sch_design_params['test_id'] + '.yaml')
         dir_name.mkdir(exist_ok=True)
@@ -49,5 +54,38 @@ def test_design_yaml(tmpdir,
         with open(expect_fname, 'r') as f:
             expect = yaml.load(f)
 
+    assert expect == actual
+
+
+def test_design_cdl(tmpdir,
+                    module_db: ModuleDB,
+                    sch_design_params: Dict[str, Any],
+                    gen_output: bool,
+                    ) -> None:
+    """Test design() method of each schematic generator."""
+    expect_fname = sch_design_params.get('out_cdl', '')
+    if not expect_fname and not gen_output:
+        pytest.skip('Cannot find expected CDL output')
+
+    dsn = get_sch_master(module_db, sch_design_params)
+
+    path = tmpdir.join('out.cdl')
+    module_db.instantiate_master(DesignOutput.CDL, dsn, top_cell_name='PYTEST', fname=str(path))
+
     assert path.check(file=1)
+
+    with path.open('r') as f:
+        actual = f.read()
+
+    if gen_output:
+        dir_name = pathlib.Path('pytest_output')
+        out_fname = dir_name / (sch_design_params['test_id'] + '.cdl')
+        dir_name.mkdir(exist_ok=True)
+        with out_fname.open('w') as f:
+            f.write(actual)
+        expect = actual
+    else:
+        with open(expect_fname, 'r') as f:
+            expect = f.read()
+
     assert expect == actual
