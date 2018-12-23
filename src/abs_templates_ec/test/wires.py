533bdd98c644bc79b8e2376e1f9742d6f446bead
Eric Chang <pkerichang@gmail.com>
2018-12-24 03:59:32 +0800

    update layout wire regression test

--- src/abs_templates_ec/test/wires.py
+++ src/abs_templates_ec/test/wires.py
@@ -20,19 +20,19 @@ class TestWire00(TemplateBase):
         # print WireArray object
         print(warr1)
         # print lower, middle, and upper coordinate of wire.
-        print(warr1.lower_unit, warr1.middle_unit, warr1.upper_unit)
+        print(warr1.lower, warr1.middle, warr1.upper)
         # print TrackID object associated with WireArray
         print(warr1.track_id)
         # add a horizontal wire on track 1, from X=0.1 to X=0.3,
         # coordinates specified in resolution units
-        self.add_wires(hm_layer, 1, 100, 300, unit_mode=True)
+        self.add_wires(hm_layer, 1, 100, 300)
         # add a horizontal wire on track 2.5, from X=0.2 to X=0.4
-        self.add_wires(hm_layer, 2.5, 200, 400, unit_mode=True)
+        self.add_wires(hm_layer, 2.5, 200, 400)
         # add a horizontal wire on track 4, from X=0.2 to X=0.4, with 2 tracks wide
-        self.add_wires(hm_layer, 4, 200, 400, width=2, unit_mode=True)
+        self.add_wires(hm_layer, 4, 200, 400, width=2)
 
         # add 3 parallel vertical wires starting on track 6 and use every other track
-        warr4 = self.add_wires(vm_layer, 6, 100, 400, num=3, pitch=2, unit_mode=True)
+        warr4 = self.add_wires(vm_layer, 6, 100, 400, num=3, pitch=2)
         print(warr4)
 
         # set the size of this template
@@ -45,4 +45,4 @@ class TestWire00(TemplateBase):
         # print bounding box of this template
         print(self.bound_box)
         # add a M7 rectangle to visualize bounding box in layout
-        self.add_rect('M7', self.bound_box)
+        self.add_rect('M7', '', self.bound_box)
